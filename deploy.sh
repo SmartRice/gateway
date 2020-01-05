2d3438988f278bd15f30baac38421a73ad78eabf
VuDA <vuda@ghn.vn>
2020-01-06 00:47:58 +0700

    correct code

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f gateway-deployment.yaml
-kubectl set image gateway-deployment server=duongvu089x/gateway:$SHA --all
+kubectl set image deployment.apps/gateway-deployment server=duongvu089x/gateway:$SHA --all
 
